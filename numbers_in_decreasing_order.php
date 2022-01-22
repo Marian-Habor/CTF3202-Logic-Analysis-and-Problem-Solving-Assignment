8e36871a0542b3c06a09eb4a19073527f380c4a6
Marian-Habor <97982783+Marian-Habor@users.noreply.github.com>
2022-01-22 12:19:58 +0000

    Create numbers_in_decreasing_order.php
    1. 	Consider the following list of numbers. Your job is to erase as few of these numbers as possible such that the remaining numbers appear in increasing order. For example, erasing everything except the first two numbers laves an increasing sequence; erasing everything except for the first, third, sixth and eighth numbers does the same.                           		                             [2]
    9 44 32 12 7 42 34 92 35 37 41 8 20 27 83 64 61 28 39 93 29 17 13 14 55 21 66 72 23 73 99 1 2 88 77 5 65 83 84 62 5 11 74 68 76 78 67 75 69 70 22 71 24 25 26
    
    2.	Solve the above exercise [1], such that the remaining numbers are in decreasing order.
    my function will solve the above question.
    
    Many thanks.
    Marian Habor.

---
+++ numbers_in_decreasing_order.php
@@ -0,0 +1,62 @@
+<?php
+// Marian Catalin Habor HE16732
+// The code is for Mr. Johnnes to review and grade.
+// PHP program to solve question number 2.
+// The given Problem "2.	Solve the above exercise [1], such that the remaining numbers are in decreasing order"
+// k elements in increasing order
+// and remaining n-k elements in
+// decreasing
+ 
+// Function to sort the array
+function printOrder($arr, $n, $k)
+{
+    $len1 = $k;
+    $len2 = $n - $k;
+    $arr1 = array_fill(0, $k, 0);
+    $arr2 = array_fill(0, ($n - $k), 0);
+ 
+    // Store the k elements
+    // in an array
+    for ($i = 0; $i < $k; $i++)
+        $arr1[$i] = $arr[$i];
+ 
+    // Store the remaining n-k
+    // elements in an array
+    for ($i = $k; $i < $n; $i++)
+        $arr2[$i - $k] = $arr[$i];
+ 
+    // sorting the array from
+    // 0 to k-1 places
+    sort($arr1);
+ 
+    // sorting the array from
+    // k to n places
+    sort($arr2);
+ 
+    // storing the values in the
+    // final array arr
+    for ($i = 0; $i < $n; $i++)
+        if ($i < $k)
+            $arr[$i] = $arr1[$i];
+ 
+        else
+        {
+            $arr[$i] = $arr2[$len2 - 1];
+            $len2 -= 1;
+    }
+     
+    // printing the array
+    for ($i = 0; $i < $n; $i++)
+        print($arr[$i] . " ");
+}
+ 
+// Driver code
+$arr = array( 9, 44, 32, 12, 7, 42, 34, 92, 35, 37, 41, 8, 20, 27, 83, 64, 61, 28, 39, 93, 29, 17, 13, 14, 55, 21, 66, 72, 23,
+73, 99, 1, 2, 88, 77, 5, 65, 83, 84, 62, 5, 11, 74, 68, 76, 78, 67, 75, 69, 70, 22, 71, 24, 25, 26, );
+$k = 4;
+ 
+$n = count($arr);
+ 
+printOrder($arr, $n, $k);
+ 
+?>
